5f29268c00443e402d9b2e06d8e4579e32f32603
Pavel Pletnev <pletnev.pg@gmail.com>
2019-05-10 18:49:03 +0300

    Change Windows font size

--- init.el
+++ init.el
@@ -63,13 +63,11 @@
    (quote
     (column-enforce-mode magit treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs use-package lsp-ui company-lsp hydra)))
  '(show-paren-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "nil" :slant normal :weight normal :height 141 :width normal)))))
 
+(if (eq system-type 'darwin)
+  (set-default-font "Consolas 14") ; OS X
+  (set-default-font "Consolas 12") ; Others
+)
 
 (setq lsp-clients-clangd-executable "/usr/local/Cellar/llvm/8.0.0/bin/clangd")
 
@@ -108,3 +106,9 @@
   ("<C-S-down>"  . buf-move-down)
   ("<C-S-left>"  . buf-move-left)
   ("<C-S-right>" . buf-move-right))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
