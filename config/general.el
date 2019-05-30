b11dfe6ca77910ecf62e59a2ec9a43b806071373
Pavel Pletnev <pletnev.pg@gmail.com>
2019-05-30 21:50:51 +0300

    Place backup files into separate folder

--- config/general.el
+++ config/general.el
@@ -1,1 +1,15 @@
+;; Save history from previous sessions
 (savehist-mode 1)
+
+;; Place backup files in separate dir
+(setq backup-directory-alist `(("." . "~/.emacs_saves")))
+
+;; Make backups by copying, may be slow
+(setq backup-by-copying t)
+
+;; Keep many backups of the same file
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
+
