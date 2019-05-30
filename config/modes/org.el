c82a0819689681f34e7d0b963287039ee648e75a
Pavel Pletnev <pletnev.pg@gmail.com>
2019-05-30 21:28:39 +0300

    Slightly customize org-mode

--- config/modes/org.el
+++ config/modes/org.el
@@ -1,10 +1,18 @@
 (require 'org)
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+
+;; Don't add timestamp when the task is done
+(setq org-log-done nil)
 
 (setq org-agenda-files (list "~/org/tickets.org"))
 
 (use-package org-bullets
   :hook
   (org-mode . org-bullets-mode))
+
+;; Prevent editing folded area
+(setq org-catch-invisible-edits 'error)
+
+;; Hide empty lines between subtrees
+(setq org-cycle-separator-lines 0)
