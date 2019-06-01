72cb23b70ef6d5a576834c2e0bd6064eb9fda262
Pavel Pletnev <pletnev.pg@gmail.com>
2019-06-01 21:18:10 +0300

    Change projectile config

--- config/modes/projectile.el
+++ config/modes/projectile.el
@@ -2,7 +2,11 @@
   :config
   (projectile-mode +1)
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (setq projectile-completion-system 'ivy)
+  (setq projectile-switch-project-action #'projectile-dired)
+  (setq projectile-indexing-method 'native))
+
 
 (use-package projectile-ripgrep
   :after projectile)
