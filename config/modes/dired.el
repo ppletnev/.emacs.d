941afbae586567b02cbd774f01ff0c4be139db72
Pavel Pletnev <pletnev.pg@gmail.com>
2019-06-04 21:13:37 +0300

    Add dired-subtree

--- config/modes/dired.el
+++ config/modes/dired.el
@@ -1,3 +1,10 @@
 
 ;; Try to guess target directory
 (setq dired-dwim-target t)
+
+;; Open directories as subtrees in current buffer
+(use-package dired-subtree
+  :after dired
+  :config
+  (bind-key "<tab>" #'dired-subtree-toggle dired-mode-map)
+  (bind-key "<backtab>" #'dired-subtree-cycle dired-mode-map))
