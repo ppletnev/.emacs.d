caeb46311bd6c81ffece3f87aa8331511350b065
Pavel Pletnev <pletnev.pg@gmail.com>
2019-06-15 20:39:37 +0300

    Minor dired improvements

--- config/modes/dired.el
+++ config/modes/dired.el
@@ -8,3 +8,18 @@
   :config
   (bind-key "<tab>" #'dired-subtree-toggle dired-mode-map)
   (bind-key "<backtab>" #'dired-subtree-cycle dired-mode-map))
+
+;; allow dired to delete or copy dir
+(setq dired-recursive-copies (quote always)) ; “always” means no asking
+(setq dired-recursive-deletes (quote top)) ; “top” means ask once
+
+;; Hide additional file info by default
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
+
+;; (use-package dired-du
+;;   :config
+;;   (add-hook 'dired-hide-details-mode-hook (lambda () (if (eq dired-hide-details-mode nil)
+;;                                                          (dired-du-mode +1)
+;;                                                        (dired-du-mode -1))))
+;;   ;; Use human-readable size format
+;;   (setq dired-du-size-format t))
