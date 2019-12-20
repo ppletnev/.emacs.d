1fe53959b9c206968370a9f07b4f6b4cdcdca17d
Pavel Pletnev <pavelpletnev@Pavels-Mac-mini.local>
2019-12-20 21:05:25 +0300

    Add smooth scrolling

--- config/ui/window.el
+++ config/ui/window.el
@@ -5,16 +5,23 @@
 ;; Disable menu-bar (bar with minor modes above)
 (menu-bar-mode -1)
 
+;; Remove tool bar
+(tool-bar-mode -1)
+
 ;; Start maximized
 ;;(add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
 
-;; Remove tool bar
-(tool-bar-mode -1)
-
 ;; Smooth scrolling on Windows
-(setq auto-window-vscroll nil)
-(setq scroll-step 1)
-(setq scroll-conservatively 10000)
-(setq mouse-wheel-scroll-amount '(3))
-(setq mouse-wheel-progressive-speed nil)
+;;(setq auto-window-vscroll nil)
+;;(setq scroll-step 1)
+;;(setq scroll-conservatively 10000)
+;;(setq mouse-wheel-scroll-amount '(3))
+;;(setq mouse-wheel-progressive-speed nil)
+
+(use-package sublimity
+  :hook (prog-mode . sublimity-mode)
+  :config
+  (require 'sublimity-scroll)
+  (setq sublimity-scroll-weight 10
+        sublimity-scroll-drift-length 6))
