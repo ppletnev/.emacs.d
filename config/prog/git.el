e40d7ff7bb8550d0c6fd4a72867715b59432eb33
Pavel Pletnev <pavelpletnev@Pavels-Mac-mini.local>
2019-12-20 17:38:56 +0300

    Add bindings for quick hunk revert/view

--- config/prog/git.el
+++ config/prog/git.el
@@ -4,8 +4,11 @@
 
 ;; Show git changes in gutter
 (use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :bind
+  ("C-c u" . git-gutter:revert-hunk)
+  ("C-c v" . git-gutter:popup-hunk)
   :config
-  (global-git-gutter-mode t)
   (setq git-gutter:added-sign " ")
   (setq git-gutter:deleted-sign " ")
   (setq git-gutter:modified-sign " "))
