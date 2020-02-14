1b0bd947378e0ed1cb87d304d28e1c6d6a48fdf8
Pavel Pletnev <luxoft.pavel-pletnev.ext@valeo.com>
2020-02-14 17:32:55 +0300

    Do not use sublimity on windows
    Change-Id: I746f69453a747a6fb3c961c738b60614b117b260

--- config/ui/window.el
+++ config/ui/window.el
@@ -13,15 +13,16 @@
 (add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
 
 ;; Smooth scrolling on Windows
-;;(setq auto-window-vscroll nil)
-;;(setq scroll-step 1)
-;;(setq scroll-conservatively 10000)
-;;(setq mouse-wheel-scroll-amount '(3))
-;;(setq mouse-wheel-progressive-speed nil)
 
-(use-package sublimity
-  :hook (prog-mode . sublimity-mode)
-  :config
-  (require 'sublimity-scroll)
-  (setq sublimity-scroll-weight 10
-        sublimity-scroll-drift-length 6))
+(if (eq system-type 'windows-nt)
+    (progn (setq auto-window-vscroll nil)
+           (setq scroll-step 1)
+           (setq scroll-conservatively 10000)
+           (setq mouse-wheel-scroll-amount '(3))
+           (setq mouse-wheel-progressive-speed nil))
+  (use-package sublimity
+    :hook (prog-mode . sublimity-mode)
+    :config
+    (require 'sublimity-scroll)
+    (setq sublimity-scroll-weight 10
+          sublimity-scroll-drift-length 6)))
