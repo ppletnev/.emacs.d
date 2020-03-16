e35da1173b133a0fd246fbb9f8116aa09db2af92
Pavel Pletnev <pletnev.pg@gmail.com>
2020-03-16 08:38:07 +0300

    Use 2-key avy jump

--- config/bindings.el
+++ config/bindings.el
@@ -4,9 +4,8 @@
 (use-package key-chord
   :config
   (key-chord-mode 1)
-  (setq key-chord-two-keys-delay 0.05)
+  (setq key-chord-two-keys-delay 0.2)
   (setq key-chord-one-key-delay 0.0)
-  (key-chord-define-global "l;" 'avy-goto-char)
-  (key-chord-define-global ";'" 'avy-go-after-char)
+  (key-chord-define-global ";'" 'avy-goto-char-2)
   (key-chord-define-global "gb" 'avy-goto-line)
   (key-chord-define-global "dc" (kbd "C-a C-SPC C-n M-w C-y C-p C-e")))
