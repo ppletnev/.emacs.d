acf46d29765dd2279a02adc33541b6d076d4f96c
Pavel Pletnev <luxoft.pavel-pletnev.ext@valeo.com>
2020-04-14 14:16:36 +0300

    Add centaur-tabs
    Change-Id: Ifae10001704aafbbb6cc40fe0fa2b382425ccc94

--- config/ui/window.el
+++ config/ui/window.el
@@ -18,3 +18,16 @@
 (setq scroll-conservatively 10000)
 (setq mouse-wheel-scroll-amount '(3))
 (setq mouse-wheel-progressive-speed nil)
+
+(use-package centaur-tabs
+  :demand
+  :config
+  (centaur-tabs-mode t)
+  (centaur-tabs-headline-match)
+  (centaur-tabs-group-by-projectile-project)
+  (setq centaur-tabs-style "bar")
+  (setq centaur-tabs-set-modified-marker t)
+  (setq centaur-tabs-modified-marker "*")
+  :bind
+  ("C-<prior>" . centaur-tabs-backward)
+  ("C-<next>" . centaur-tabs-forward))
